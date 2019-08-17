2710b80a412e53a719e6e1ac7df605f262103900
Rex <im@rex.tw>
2019-08-18 01:02:10 +0800

    增加專案建置成war檔 參考: https://coffee0127.github.io/blog/2017/04/26/convert-spring-boot-jar-to-war/

--- src/main/java/tw/rex/java/spring/SpringMvCuseBootApplication.java
+++ src/main/java/tw/rex/java/spring/SpringMvCuseBootApplication.java
@@ -2,6 +2,8 @@ package tw.rex.java.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 //相依注入設定在myMVC-servlet.xml，這裡註解掉
 //import org.springframework.context.annotation.ComponentScan;
@@ -9,8 +11,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 //相依注入設定在myMVC-servlet.xml，這裡註解掉 
 //@ComponentScan(basePackages={"tw.rex.java.spring.controller","tw.rex.java.spring.service"})
-public class SpringMvCuseBootApplication {
+public class SpringMvCuseBootApplication extends SpringBootServletInitializer{
 
+	//參考https://coffee0127.github.io/blog/2017/04/26/convert-spring-boot-jar-to-war/
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SpringMvCuseBootApplication.class);
+    }
+    
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMvCuseBootApplication.class, args);
 	}
